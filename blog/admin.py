47a0f23720165a7d564fac3c56eea22c7fd1d92b
neulhan <sting7675@gmail.com>
2019-07-19 19:04:19 +0900

    blog.models.Post 모델에 status 필드를 추가하고, 이를 처리하는 Custom Admin Action 구현

--- blog/admin.py
+++ blog/admin.py
@@ -6,11 +6,24 @@ from .models import Post
 # Register your models here.
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'content_size', 'created_at', 'author']
+    list_display = ['id', 'title', 'status', 'content_size', 'created_at', 'author']
     list_display_links = ['id', 'title', 'created_at', 'author']
     list_per_page = 5
+    actions = ['make_published', 'make_draft']
 
     def content_size(self, post):
         return '{}글자'.format(len(post.content))
+
     content_size.short_description = "내용 글자수"
+
+    def make_published(self, request, queryset):
+        updated_count = queryset.update(status='p')
+        self.message_user(request, '{}건의 포스팅을 published 상태로 변경'.format(updated_count))
+    make_published.short_description = " 지정 포스팅을 published 상태로 변경합니다."
+
+    def make_draft(self, request, queryset):
+        updated_count = queryset.update(status='d')
+        self.message_user(request, '{}건의 포스팅을 draft 상태로 변경'.format(updated_count))
+
+    make_draft.short_description = " 지정 포스팅을 draft 상태로 변경합니다."
 # admin.site.register(Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -11,13 +11,19 @@ def lnglat_validator(value):
 
 
 class Post(models.Model):
+    STATUS_CHOICES = (
+        ('d', 'draft'),
+        ('p', 'published'),
+        ('w', 'withdraw'),
+    )
+
     title = models.CharField(max_length=100, help_text="포스팅 제목을 입력해 주세요", verbose_name='제목')  # 길이 제한이 있는 문자열
     content = models.TextField()  # 길이 제한이 없는 문자열
     reference = models.CharField(max_length=150, default=None)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)  # DateTimeField 는 자동으로 설정됨
     tags = models.CharField(max_length=100, blank=True)
-    lnglat = models.CharField(max_length=50, validators=[lnglat_validator], blank=True, help_text="위도,경도 포맷으로 입력")
     author = models.CharField(max_length=50)
     anony = models.TextField(default=None)
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES)
 
