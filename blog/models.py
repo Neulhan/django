a7cb583fd1a53166651ce4f0f04ec2545c0e30c3
neulhan <sting7675@gmail.com>
2019-07-19 16:42:19 +0900

    blog.models.Post모델에 옵션필드인 lnglat/tag 필드 추가

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,20 @@
 # blog/models.py
 from django.db import models
+import re
+from django.forms import ValidationError
 
 
 # Create your models here.
+def lnglat_validator(value):
+    if not re.match(r'^(\d+\.?\d*),(\d+\.?\d*)$', value):
+        raise ValidationError("유효하지 않은 입려값입니다")
+
 
 class Post(models.Model):
-    title = models.CharField(max_length=100,help_text="포스팅 제목을 입력해 주세요", verbose_name='제목')  # 길이 제한이 있는 문자열
+    title = models.CharField(max_length=100, help_text="포스팅 제목을 입력해 주세요", verbose_name='제목')  # 길이 제한이 있는 문자열
     content = models.TextField()  # 길이 제한이 없는 문자열
     reference = models.CharField(max_length=150, default=None)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)  # DateTimeField 는 자동으로 설정됨
+    tags = models.CharField(max_length=100, blank=True)
+    lnglat = models.CharField(max_length=50, validators=[lnglat_validator], blank=True, help_text="위도,경도 포맷으로 입력")
