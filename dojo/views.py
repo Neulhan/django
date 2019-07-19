f6815a66edabeca00f50befe1c46b7eb32146caa
neulhan <sting7675@gmail.com>
2019-07-19 11:47:10 +0900

    dojo.views.hello:한글 문자열 매칭

--- dojo/views.py
+++ dojo/views.py
@@ -3,8 +3,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def mysum(request, numbers):
+def mysum(request, name, age):
     #request: HttpRequest
-    result = sum(map(lambda s: int(s or 0), numbers.split("/")))
-    return HttpResponse(result)
+    return HttpResponse('안녕하세요.{}.{}살이시네요.'.format(name, age))
+
 
