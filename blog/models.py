a25222378491dc8e5ae2a4e4e8c49e00b1634f85
neulhan <sting7675@gmail.com>
2019-07-23 12:07:14 +0900

    related_name 예시로서, shop.models.Post 모델 생성

--- blog/models.py
+++ blog/models.py
@@ -19,7 +19,8 @@ class Post(models.Model):
         ('p', 'published'),
         ('w', 'withdraw'),
     )
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
+                             related_name='blog_post_set')
     title = models.CharField(max_length=100, help_text="포스팅 제목을 입력해 주세요", verbose_name='제목')  # 길이 제한이 있는 문자열
     content = models.TextField()  # 길이 제한이 없는 문자열
     reference = models.CharField(max_length=150, default=None)
