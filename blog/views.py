7f6cc6e80034a776c6586815d7124928bd47db4b
neulhan <sting7675@gmail.com>
2019-07-27 17:09:01 +0900

    dojo.models.Post 모델을 추가하고, 마이그레이션

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 from .models import Post
 
 # Create your views here.
+
+
 def post_list(request):
     qs = Post.objects.all()
     return render(request, "blog/post_list.html",{
--- dojo/views.py
+++ dojo/views.py
@@ -1,38 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-import os
-from django.conf import settings
-# Create your views here.
+from django.http import HttpResponse
+
 
 def mysum(request, name, age):
-    #request: HttpRequest
+
     return HttpResponse('안녕하세요.{}.{}살이시네요.'.format(name, age))
 
-def post_list1(request):
-    name = "신한결"
-    return HttpResponse("""
-    <h1>AskDjango</h1>
-    <p>{name}</p>
-    <p>장고를 배우는 늘한입니다</p>
-    """.format(name=name))
-
-def post_list2(request):
-    name = "늘한"
-    return render(request, 'dojo/post_list.html',{'name':name})
-
-def post_list3(request):
-    return JsonResponse(
-        {'message': '안녕 파이썬&장고',
-         'items': ['파이썬',"장고", "celery", 'azure', 'aws'],
-         }
-        ,json_dumps_params={'ensure_ascii':False}
-    )
-
-def excel_download(request):
-    filepath = os.path.join(settings.BASE_DIR, 'm.xls')
-    filename = os.path.basename(filepath)
-    with open(filepath, 'rb') as f:
-        response = HttpResponse(f, content_type='application/vnd.ms-excel')
-        response['Content-Disposition'] = 'attachment; filename="{}"'.format(filename)
-        return response
- 
