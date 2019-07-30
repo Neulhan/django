0f507019171f94040ca6cf7a9a69d05b7d1e1c25
neulhan <sting7675@gmail.com>
2019-07-30 14:13:03 +0900

    dogo.forms.min define

--- dojo/urls.py
+++ dojo/urls.py
@@ -5,8 +5,4 @@ from . import views_cbv
 
 urlpatterns = [
     re_path(r'^hello/(?P<name>[ㄱ-힣]+)/(?P<age>\d+)/$', views.mysum),
-    re_path(r'^list1/$', views.post_list1),
-    re_path(r'^list2/$', views.post_list2),
-    re_path(r'^list3/$', views.post_list3),
-    re_path(r'^excel/$', views.excel_download),
 ]
